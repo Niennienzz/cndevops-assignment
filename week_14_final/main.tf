dcb871c1259d6723bf6a880cb273c1d319f6e606
Joe Zhou <zhehui.zhou@outlook.com>
2023-12-20 17:42:30 -0500

    feat: add k3s

--- week_14_final/main.tf
+++ week_14_final/main.tf
@@ -1,10 +1,17 @@
 module "cvm" {
-  source = "./modules/cvm"
-  secret_id = var.secret_id
-  secret_key = var.secret_key
-  password = var.password
-  memory = 16
-#  vpc_id = module.vpc.vpc_id
-#  subnet_id = module.vpc.subnet_id
+  source        = "./modules/cvm"
+  secret_id     = var.secret_id
+  secret_key    = var.secret_key
+  password      = var.password
+  memory        = 16
+  #  vpc_id = module.vpc.vpc_id
+  #  subnet_id = module.vpc.subnet_id
   instance_name = "k8s"
+}
+
+module "k3s" {
+  source      = "./modules/k3s"
+  public_ip   = module.cvm.public_ip
+  private_ip  = module.cvm.private_ip
+  server_name = "k3s-hongkong-1"
 }
